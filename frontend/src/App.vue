3442e697443c0e17a7676c9ab8f6224f06d3d444
Everton Tenorio <etenoriodev@gmail.com>
2024-02-24 14:42:53 -0300

    api estadios-alagoas

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -54,7 +54,7 @@ export default {
     
 
     // Requisição para obter os dados das localidades
-    axios.get('http://evertontenorio.tech:3001/api/localidades')
+    axios.get('https://jogos.evertontenorio.tech/api/localidades')
       .then(response => {
         this.localidades = response.data;
         // Adiciona marcadores ao mapa para cada localidade
